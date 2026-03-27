42a4d4a4531fbb7e6f452070deca052916f489f2
Djpatel00 <dakshjpatel007@gmail.com>
2026-03-27 22:08:04 +0530

    setting up the CICD Pipeline

--- client/pages/bana.js
+++
@@ -1,5 +0,0 @@
-const ban=()=>{
-    return <h1>Bana 2</h1>
-}
-
-export default ban;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,13 @@
-import axios from 'axios';
-
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
-
-    return <h1>Landing Page</h1>;
+    return currentUser ? (
+        <h1>You are signed in.</h1>
+    ) : (
+        <h1>You are not signed in</h1>
+    )
 };
 
-LandingPage.getInitialProps = async () => {
-    if (typeof window === 'undefined') {
-        // we are on the server
-        const response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-            {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
-            }
-        )
-        console.log(response.data);
-        return response.data;
-    } else {
-        // we are on the browser
-        const { data } = await axios.get('/api/users/currentuser')
-        return data;
-    }
+LandingPage.getInitialProps = async (context,client,currentUser) => {
+    return {};
 }
 
 export default LandingPage;
